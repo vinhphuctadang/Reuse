f450e9cc4b54ff67e726f7f578fc57c6e4f3e6d2
vinhphuctadang <30641530+vinhphuctadang@users.noreply.github.com>
2019-04-13 21:06:08 +0700

    Update IrisVisual.py

--- IrisVisual.py
+++ IrisVisual.py
@@ -1,5 +1,6 @@
 # this code will show how simple it is to plot a decision tree (or to visualize it :))
 # 1st: Install GraphViz, I have already done downloading 
+# setting system 'PATH' on windows in case it (the graphviz) does not recognize it
 # 2st: Coding:
 
 from sklearn.tree import export_graphviz
