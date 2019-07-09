07001f9e34add7e864c8c42f70f50f27871eee8c
vinhphuctadang <30641530+vinhphuctadang@users.noreply.github.com>
2019-07-09 14:35:54 +0700

    Beta, tested

--- Describe/describe.cpp
+++ Describe/describe.cpp
@@ -1,5 +1,5 @@
 #include "StringUtils.h"
-//#include "cxxopts/include/cxxopts.hpp"
+#include "cxxopts/include/cxxopts.hpp"
 #include <windows.h>
 #include <cstring>
 #include <stdio.h>
@@ -62,31 +62,16 @@ void walk (const string& source, vector<string>& files, vector<string>& dirs) {
    		throw DescribeException ("NO MORE FILE", -1);
 }
 
-
-json collect (const string& parentDir, const string& dir) {
-	
-	json result;
-	vector<string> files, dirs;
-	auto theDir = parentDir + "\\" + dir;
-	walk (theDir, files, dirs);
-	
-	result["name"] = dir;
-	
-	vector<json> discription;
-	
-	for (auto name:files) {
-		json temp;
-		temp["name"] = name;
-		discription.push_back(temp);
-	}
-	
-	for (auto name: dirs) {
-		json temp = collect (theDir, name); // recursively call
-		discription.push_back(temp);
-	}
-	
-	result ["content"] = discription;
-	return result;
+regex toRegex (const string& pattern) {
+	string proccessed_pattern = "";
+	for (int i = 0; i<pattern.length(); ++i) {
+		if (pattern[i] == '.' || pattern[i] == '\\')
+			proccessed_pattern += "\\";
+		else if	(pattern[i] == '*' || pattern[i] == '?' || pattern[i]=='^')
+			proccessed_pattern += string("."); 
+		proccessed_pattern += pattern[i];
+ 	} 
+	return regex (proccessed_pattern);	
 }
 
 json load (const string& name) {
@@ -102,14 +87,159 @@ json load (const string& name) {
 
 void save (const string& name, json& j) {
 	ofstream file(name, ios_base::out);
-
 	if (file.good ()) {
 		file << j;
 	}
 	file.close ();
 }
 
-int main(int argc, char *argv[]) {
+json collect (const string& root, const string& current, 
+	vector<regex>& filter, 
+	vector<regex>& ignore,
+	const string& virtual_dir // for regex to check matching expression pattern with ease
+	) {
+		
+	json result;
+	vector<string> files, dirs;
+	auto theDir = root + "\\" + current;
+	walk (theDir, files, dirs);
 	
+	result["name"] = current;
+	vector<json> discription;
+	
+	for (auto name:files) {		
+		
+		bool cont = 0;
+		
+		for (auto reg : ignore) {
+			if (regex_match (name, reg)) {
+				cont = 1;
+				break;
+			}
+		}
+		if (cont) continue;
+		
+		if (filter.size()) {
+			for (auto reg : filter)
+				if (regex_match (name, reg)) {
+					json temp;
+					temp["name"] = name;
+					discription.push_back(temp);
+					break;
+				}
+		} else {
+//			printf ("%s\n", name.c_str ());	
+			json temp;
+			temp["name"] = name;
+			discription.push_back(temp);
+		}
+	}
+	
+	for (auto name: dirs) {
+		bool cont = 0;
+		for (auto reg : ignore) {
+			
+			if (regex_match (virtual_dir+"\\"+name, reg)) { 
+				cont = 1;
+				break;
+			}
+		}
+		if (cont) continue;
+		
+		json temp = collect (theDir, name, filter, ignore, virtual_dir+"\\"+name); // recursively call
+		discription.push_back(temp);
+	}
+	
+	result ["content"] = discription;
+	return result;
+}
+
+cxxopts::Options init (char* program) {
+	cxxopts::Options options(program, " - describe current directory to json file *.drive");
+	options.add_options()
+		("p,path", "Origianl path to describe, '.' by default", cxxopts::value<std::string>())
+  		("i,ignore", "Ignore file/folder", cxxopts::value<vector<string>>())
+  		("f,filter", "Filter (only accept)", cxxopts::value<vector<string>>())
+		("o,output", "File name", cxxopts::value<string>())
+		("m,main", "Main file (for execution, optional)", cxxopts::value<std::string>())
+		("h,help", "Show help", cxxopts::value<bool>())
+		;	
+	return options;
+}
+
+void input (cxxopts::ParseResult& parsed, vector<regex>& filter, vector<regex>& ignore, string& root, string& main_value, string& oFile) {
+	
+	try {
+		root = parsed ["p"].as <string> ();		
+	} catch (exception& e) {
+		root = ".";
+	}
+		
+	
+	try {
+		oFile = parsed ["o"].as <string> ();		
+	} catch (exception& e) {
+		oFile = ".drive";
+	}
+		
+	try {
+		main_value = parsed ["m"].as <string> ();		
+	} catch (exception& e) {
+		main_value = "";
+	}	
+		
+	vector<string> vec;
+	try {
+		vec = parsed ["f"].as <vector<string>> ();
+		for (auto element : vec) {
+			filter.push_back(toRegex (element));
+		}
+	} catch (exception& e) {
+		filter = {};
+	}
+		
+	try {
+		vec = parsed ["i"].as <vector<string>> ();
+		for (auto element : vec) {
+			printf ("%s\n", element.c_str ());
+			ignore.push_back(toRegex (element));
+		}
+	} catch (exception& e) {
+		ignore = {};
+	}
+			
+}
+
+json run (vector<regex>& filter, vector<regex>& ignore, string& root, string& main_value) {
+	auto result = collect (".", root, filter, ignore, "");
+	if (main_value != "")
+		result ["main"] = main_value;
+	return result;
+}
+
+void done (json& j, const string& file) {
+	save (file, j);
+}
+
+int main(int n, char** argv) {
+	
+	// "describe -o myfolder.drive -f *.py,*.exe,*.ini,*.json -i \content -i file*"
+	vector<regex> filter, ignore;
+	string root, main_value, oFile;
+	auto options = init (argv[0]);
+	auto parsed = options.parse (n, argv);
+	bool h = false;
+	try {
+		h = parsed["h"].as <bool> ();
+		if (h) {
+			printf ("%s", options.help (options.groups ()).c_str ());
+			return 0;
+		}
+	} catch (exception& e) {
+	}
+	
+	input (parsed, filter, ignore, root, main_value, oFile);
+	auto result = run (filter, ignore, root, main_value);
+	done (result, root + "\\" + oFile);		
 	return 0;
 }
