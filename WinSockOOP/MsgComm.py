5a496bbdeac7309a2c860134b12cc7032a162d66
vinhphuctadang <30641530+vinhphuctadang@users.noreply.github.com>
2019-07-10 23:16:47 +0700

    Client library for python

--- WinSockOOP/MsgComm.py
+++ WinSockOOP/MsgComm.py
@@ -3,16 +3,22 @@ import socket
 MSG_HEADER_SIZE = 4
 
 class MsgComm (socket.socket):  
-	
+	def __init__ (self,HOST='127.0.0.1',PORT=16472):
+		socket.socket.__init__ (self, socket.AF_INET, socket.SOCK_STREAM);
+		self.HOST = HOST
+		self.PORT = PORT
+	def connect (self):
+		socket.socket.connect (self, (self.HOST, self.PORT))
+
 	def sendMsg (self, msg):
 		header = len (msg)
 		self.send (header.to_bytes (MSG_HEADER_SIZE, byteorder='little'))
-		self.send (bytes(msg))
+		self.send (bytes(msg, 'utf-8'))
 
 	def recvMsg (self, bufferSize=1024, maxSize=1000000000):
 		header =  self.recv (MSG_HEADER_SIZE);
 		size = int.from_bytes (header, byteorder='little', signed=False);
-		print (size)
+		# print (size)
 
 		if (size > maxSize):
 			raise ValueError ('Overflow size of %d' % size)
@@ -25,8 +31,6 @@ class MsgComm (socket.socket):
 				size -= min (bufferSize, size, len (msg))
 				collection += msg
 			return collection
-		
-
 
 def main ():
 	HOST = '127.0.0.1'  # The server's hostname or IP address
