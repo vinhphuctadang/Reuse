afabb1406d543b8382388d245692797d59c53b72
vinhphuctadang <30641530+vinhphuctadang@users.noreply.github.com>
2019-07-17 22:55:56 +0700

    Main file for execution: Shark.py

---
+++ HNhuClient/shark.py
@@ -0,0 +1,21 @@
+import sys
+import argparse 
+import HNhu
+import json
+from jsonplay import toDict, present
+
+def main ():
+
+	args = toDict (sys.argv[1:]) # Convert command line to dictionairy
+	try:
+		nhu = HNhu.HNhu ()
+	except Exception as e:
+		print (e.what (), '. Stopped')
+		return 
+
+	result = nhu.request (args)
+
+	present (result)
+
+if __name__=='__main__':
+	main ()
