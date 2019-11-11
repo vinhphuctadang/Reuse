6c256ea0ad9af01c19bc27cd6ffddb31c9bcf53f
vinhphuctadang <30641530+vinhphuctadang@users.noreply.github.com>
2019-11-11 22:47:49 +0700

    Update procon.py

--- ProconAPIWrapper/procon.py
+++ ProconAPIWrapper/procon.py
@@ -2,22 +2,31 @@
 import json
 import requests 
 import os
+
+TOKEN = 'procon30_example_token'
+HOST = "http://127.0.0.1:55531"; # Đặt server thật vào đây
+CURRENT_MATCH_URL = "/matches/1"; # Đổi số hiệu trận đấu hiện tại ở đây
 '''
 	httpGET: Tạo một http request GET lên server
+	hiện tại do ta chỉ quan tâm đến server chấm thi nên HOST được sử dụng trong hàm này và URL là đường dẫn TƯƠNG ĐỐI đến chỗ cần lấy
 '''
-HOST = "http://127.0.0.1:51145/";
-
 def httpGET (URL, PARAMS):	
 	# api-endpoint 
 	# sending get request and saving the response as response object 
-	requestString = 'curl --silent -H "Authorization: procon30_example_token" %s%s'%(HOST, URL);
+	requestString = 'curl --silent -H "Authorization: %s" %s%s'%(TOKEN, HOST, URL);
+	# print (requestString)
 	result = os.popen (requestString).read ();
 	return json.loads (result);
+
+'''
+	httpPOST: Tạo một http request POST lên server
+	tương tự, hiện tại do ta chỉ quan tâm đến server chấm thi nên HOST được sử dụng trong hàm này và URL là đường dẫn TƯƠNG ĐỐI đến chỗ cần lấy
+'''
 
 def httpPOST (URL, PARAMS):	
 	# api-endpoint 
 	# sending get request and saving the response as response object 
-	header = 'curl --silent -H "Authorization: procon30_example_token" -H Content-Type:application/json %s%s' % (HOST, URL);
+	header = 'curl --silent -H "Authorization: %s" -H Content-Type:application/json %s%s' % (TOKEN, HOST, URL);
 	jsonRq = json.dumps (PARAMS);
 	parsed = json.dumps (jsonRq);
 	# print (parsed);
@@ -27,34 +36,28 @@ def httpPOST (URL, PARAMS):
 	result = os.popen (requestString).read ();
 	return json.loads (result);
 
-def sendActions (URL, lst):
+def sendActions (lst):
 	toSend = {"actions":lst}
-	ans = httpPOST (URL, toSend)
+	ans = httpPOST (CURRENT_MATCH_URL, toSend)
 	return ans
 
 def __action (agentID, action, dx=1, dy=0):
 	toReturn = {
-
 		"agentID" : agentID,
 		"type" : action,
 		"dx": dx,
 		"dy": dy
 	}
 	return toReturn;
-def __agent (agentID):
-	pass
-
+def getMatch ():
+	result = httpGET (CURRENT_MATCH_URL, {})
+	return result;
+	# pass
 def main ():
-	URL = '/matches/1/action'	
-	lst = [
-		__action (2, "move", dx=1, dy=1),
-		__action (3, "move", dx=-1, dy=-1),
-	];
-
-	print (sendActions (URL, lst))
-
-	URL = '/matches/1';
-	print (httpGET (URL, {}))
+	acts = [
+		__action (2, "move", dx=1, dy=0)
+	]
+	print (getMatch())
 
 if __name__=="__main__":
 	main ()
